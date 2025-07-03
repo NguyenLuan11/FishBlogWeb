96c607ad70d1ac9a3e8bf51626104b681ff6bc2c
NguyenLuan11 <nguyenluan0101020@gmail.com>
2025-07-03 15:26:47 +0700

    add config HiddenHttpMethodFilter to handle form HTML(only have POST, GET)

---
+++ src/main/java/com/tnluan/fishblogweb/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.tnluan.fishblogweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean<HiddenHttpMethodFilter> hiddenHttpMethodFilter() {
+        FilterRegistrationBean<HiddenHttpMethodFilter> filterRegistrationBean = new FilterRegistrationBean<>(new HiddenHttpMethodFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+}
